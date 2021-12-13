f3b65138acdd54111bbc181b7c3df0050c646756
JOHNW02 <wei33@illinois.edu>
2021-12-13 15:38:59 -0600

    add two more layers to cnn

--- dl_model_interpretation.py
+++ dl_model_interpretation.py
@@ -26,134 +26,169 @@ import torch.nn as nn
 import torch.nn.functional as F
 from dataset import Dataset
 
-random_seed=42
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-torch.cuda.set_device(device)
-torch.cuda.empty_cache()
+if __name__ == '__main__':
 
-# load original metadata
-df = pd.read_csv("trainLabels_cropped.csv")
-image_names = df['image'].tolist()
-y = df['level']
+	parser = argparse.ArgumentParser(description="Execute Train")
+	parser.add_argument('--deep', type=str, default="True", help="...")
+    parser.add_argument('--ckpt', type=str, default="True", help="...")
+	args = parser.parse_args()
 
-subject0 = df.loc[ df['level']==0][0:10000]
-subject1 = df.loc[ df['level']==1]
-subject2 = df.loc[ df['level']==2]
-subject3 = df.loc[ df['level']==3]
-subject4 = df.loc[ df['level']==4]
+	if args.deep=="True":
+		depth = True
+	else if args.deep=="False":
+		depth = False
+	else 
+		raise ValueError("unknown!")
 
-# drop class 0 to 10000
-df = pd.concat( (subject0, subject1, subject2, subject3, subject4), axis=0, ignore_index=True )
-y = df['level'].tolist()
-image_names = df['image']
-index = np.arange(len(y))
+    random_seed=42
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    torch.cuda.set_device(device)
+    torch.cuda.empty_cache()
 
-# load preprocessed images
-X = joblib.load("X")
-print(X.shape)
-X = np.swapaxes(X, 1, 3)
-print(X.shape)
+    # load original metadata
+    df = pd.read_csv("trainLabels_cropped.csv")
+    image_names = df['image'].tolist()
+    y = df['level']
 
-# make train, valid, heldout splits
-train_val_indices, heldout_indices = train_test_split(index, test_size=0.3, random_state=random_seed, stratify=y)
+    subject0 = df.loc[ df['level']==0][0:10000]
+    subject1 = df.loc[ df['level']==1]
+    subject2 = df.loc[ df['level']==2]
+    subject3 = df.loc[ df['level']==3]
+    subject4 = df.loc[ df['level']==4]
 
-# Load pretrained resnet 18. Here we only take 1 block of resnet 18 and add 2 more conv layers and 1 classification layer.
-model = resnet18(pretrained=True)
-model = torch.nn.Sequential(*(list(model.children())[0:5]),
-                nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False),
-                nn.Conv2d(64, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
-                nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(32, 16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
-                nn.AdaptiveAvgPool2d(output_size=(1, 1)),
-                nn.Flatten(1, -1),
-                nn.Linear(16, 5, bias=True)
-                )
-# Load model state
-ckpt = torch.load("model_final.ckpt", map_location=device)
-model.load_state_dict(ckpt['model_state'])
-model = model.to(device)
+    # drop class 0 to 10000
+    df = pd.concat( (subject0, subject1, subject2, subject3, subject4), axis=0, ignore_index=True )
+    y = df['level'].tolist()
+    image_names = df['image']
+    index = np.arange(len(y))
+
+    # load preprocessed images
+    X = joblib.load("X")
+    print(X.shape)
+    X = np.swapaxes(X, 1, 3)
+    print(X.shape)
+
+    # make train, valid, heldout splits
+    train_val_indices, heldout_indices = train_test_split(index, test_size=0.3, random_state=random_seed, stratify=y)
+
+    # Load pretrained resnet 18. Here we only take 1 block of resnet 18 and add 2 more conv layers and 1 classification layer.
+    model = resnet18(pretrained=True)
+    if depth:
+		model = torch.nn.Sequential(*(list(model.children())[0:5]),
+										nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False),
+										nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+										nn.ReLU(inplace=True),
+										nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+										nn.ReLU(inplace=True),
+										nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+										nn.ReLU(inplace=True),
+										nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+										nn.Flatten(1, -1),
+										nn.Linear(128, 5, bias=True)
+										)
+	else:
+		model = torch.nn.Sequential(*(list(model.children())[0:5]),
+										nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False),
+										nn.Conv2d(64, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+										nn.ReLU(inplace=True),
+										nn.Conv2d(32, 16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+										nn.Flatten(1, -1),
+										nn.Linear(16, 5, bias=True)
+										)
+    
+    # Load model state
+    ckpt = torch.load(args.ckpt, map_location=device)
+    model.load_state_dict(ckpt['model_state'])
+    model = model.to(device)
+
+    print(model)
+
+    heldout_dataset = Dataset(X, y, heldout_indices)
+    heldout_dataloader = DataLoader(heldout_dataset, batch_size=32, shuffle=False)
+
+    # Evaluate on heldout set
+    model.eval()
+    with torch.no_grad():
+        y_probs_valid = torch.empty(0, 5).to(device)
+        y_true_valid, y_pred_valid = [], []
+
+        for i, batch in enumerate(heldout_dataloader):
+            X_batch = batch["data"].to(device)
+            y_batch = batch["label"].to(device)
+            
+            outputs = model(X_batch.float())
+
+            _, predicted = torch.max(outputs.data, 1)
+            y_pred_valid += predicted.cpu().numpy().tolist()
+            y_probs_valid = torch.cat((y_probs_valid, outputs), 0)
+            y_true_valid += y_batch.cpu().numpy().tolist()
+        y_probs_valid = F.softmax(y_probs_valid, dim=1).cpu().numpy()
+        y_true_valid = np.array(y_true_valid)
+
+    auc = roc_auc_score(y_true_valid, y_probs_valid, multi_class='ovr')
+    print("AUC: ", auc)
+    print(classification_report(y_true_valid, y_pred_valid))
 
 
-heldout_dataset = Dataset(X, y, heldout_indices)
-heldout_dataloader = DataLoader(heldout_dataset, batch_size=32, shuffle=False)
+    X_heldout = X[heldout_indices]
+    y_pred_valid = np.array(y_pred_valid)
 
-# Evaluate on heldout set
-model.eval()
-with torch.no_grad():
-    y_probs_valid = torch.empty(0, 5).to(device)
-    y_true_valid, y_pred_valid = [], []
 
-    for i, batch in enumerate(heldout_dataloader):
-        X_batch = batch["data"].to(device)
-        y_batch = batch["label"].to(device)
+    # Grad Cam
+    # Find predicted=True images and extract the features
+    true_0_idx = []
+    true_1_idx = []
+    true_2_idx = []
+    true_3_idx = []
+    true_4_idx = []
+    for i in range(y_true_valid.shape[0]):
+        if y_pred_valid[i] == 0 and y_true_valid[i] == 0:
+            true_0_idx.append(i)
+        if y_pred_valid[i] == 1 and y_true_valid[i] == 1:
+            true_1_idx.append(i)
+        if y_pred_valid[i] == 2 and y_true_valid[i] == 2:
+            true_2_idx.append(i)
+        if y_pred_valid[i] == 3 and y_true_valid[i] == 3:
+            true_3_idx.append(i)
+        if y_pred_valid[i] == 4 and y_true_valid[i] == 4:
+            true_4_idx.append(i)
+
+    # We choose 5 images, that their predictions are correct.
+    to_produce_list = [true_0_idx[0], true_1_idx[7], true_2_idx[2], true_3_idx[0], true_4_idx[1] ]
+    heldout_img_idx = heldout_indices[to_produce_list]
+    img_names = image_names[heldout_img_idx]
+    print(img_names)
+    target_layers = [model[9]]
+
+    for i in range( len(to_produce_list) ):
+
+        img_idx = to_produce_list[i]
+
+        image_0 = np.float32(np.expand_dims(X_heldout[ img_idx ], axis=0) )
+
+        input_tensor = torch.tensor(image_0).to(device).float()
+
+        cam = GradCAM(model=model, target_layers=target_layers, use_cuda=True)
+
+        target_category = i
+
+        # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
+        grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category, aug_smooth=True)
+
+        # In this example grayscale_cam has only one image in the batch:
+        grayscale_cam = grayscale_cam[0, :]
+
+        # Swap axes back to BGR
+        to_test_image = np.swapaxes( np.float32(X_heldout[ img_idx ]), 0, 2)
         
-        outputs = model(X_batch.float())
+        cam_image = show_cam_on_image(to_test_image, grayscale_cam, use_rgb=True)
 
-        _, predicted = torch.max(outputs.data, 1)
-        y_pred_valid += predicted.cpu().numpy().tolist()
-        y_probs_valid = torch.cat((y_probs_valid, outputs), 0)
-        y_true_valid += y_batch.cpu().numpy().tolist()
-    y_probs_valid = F.softmax(y_probs_valid, dim=1).cpu().numpy()
-    y_true_valid = np.array(y_true_valid)
-
-auc = roc_auc_score(y_true_valid, y_probs_valid, multi_class='ovr')
-print("AUC: ", auc)
-print(classification_report(y_true_valid, y_pred_valid))
-
-
-X_heldout = X[heldout_indices]
-y_pred_valid = np.array(y_pred_valid)
-
-
-# Grad Cam
-true_0_idx = []
-true_1_idx = []
-true_2_idx = []
-true_3_idx = []
-true_4_idx = []
-for i in range(y_true_valid.shape[0]):
-    if y_pred_valid[i] == 0 and y_true_valid[i] == 0:
-        true_0_idx.append(i)
-    if y_pred_valid[i] == 1 and y_true_valid[i] == 1:
-        true_1_idx.append(i)
-    if y_pred_valid[i] == 2 and y_true_valid[i] == 2:
-        true_2_idx.append(i)
-    if y_pred_valid[i] == 3 and y_true_valid[i] == 3:
-        true_3_idx.append(i)
-    if y_pred_valid[i] == 4 and y_true_valid[i] == 4:
-        true_4_idx.append(i)
-
-# We choose 5 images, that their predictions are correct.
-to_produce_list = [true_0_idx[0], true_1_idx[0], true_2_idx[0], true_3_idx[0], true_4_idx[0] ]
-heldout_img_idx = heldout_indices[to_produce_list]
-img_names = image_names[heldout_img_idx]
-print(img_names)
-target_layers = [model[6]]
-
-for i in range( len(to_produce_list) ):
-
-    img_idx = to_produce_list[i]
-
-    image_0 = np.float32(np.expand_dims(X_heldout[ img_idx ], axis=0) )
-
-    input_tensor = torch.tensor(image_0).to(device).float()
-
-    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=True)
-
-    target_category = i
-
-    # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
-    grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category, aug_smooth=True)
-
-    # In this example grayscale_cam has only one image in the batch:
-    grayscale_cam = grayscale_cam[0, :]
-
-    # Swap axes back to BGR
-    to_test_image = np.swapaxes( np.float32(X_heldout[ img_idx ]), 0, 2)
-    
-    cam_image = show_cam_on_image(to_test_image, grayscale_cam, use_rgb=True)
-
-    cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
-    
-    cv2.imwrite(f'class_{i}_cam.jpg', cam_image)
+        cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
+        
+        cv2.imwrite(f'class_{i}_cam.jpg', cam_image)
--- dl_pipeline.py
+++ dl_pipeline.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import os
 import joblib
+import argparse
+import parser
 from tqdm import tqdm
 from collections import Counter
 from sklearn.neighbors import KNeighborsClassifier
@@ -22,131 +24,180 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from dataset import Dataset
-# set seed
-random_seed=42
 
-# load original metadata
-df = pd.read_csv("trainLabels_cropped.csv")
-image_names = df['image']
-y = df['level']
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description="Execute Train")
+	parser.add_argument('--deep', type=str, default="True", help="...")
+	parser.add_argument('--pretrain', type=str, default="True", help="...")
+	args = parser.parse_args()
+
+	if args.deep=="True":
+		depth = True
+	else if args.deep=="False":
+		depth = False
+	else 
+		raise ValueError("unknown!")
+
+	if args.pretrain=="True":
+		Pre_Train = True
+	else if args.pretrain=="False":
+		Pre_Train = False
+	else 
+		raise ValueError("unknown!")
+
+	# set seed
+	random_seed=42
+
+	# load original metadata
+	df = pd.read_csv("trainLabels_cropped.csv")
+	image_names = df['image']
+	y = df['level']
 
 
-subject0 = df.loc[ df['level']==0][0:10000]
-subject1 = df.loc[ df['level']==1]
-subject2 = df.loc[ df['level']==2]
-subject3 = df.loc[ df['level']==3]
-subject4 = df.loc[ df['level']==4]
+	subject0 = df.loc[ df['level']==0][0:10000]
+	subject1 = df.loc[ df['level']==1]
+	subject2 = df.loc[ df['level']==2]
+	subject3 = df.loc[ df['level']==3]
+	subject4 = df.loc[ df['level']==4]
 
-# drop class 0 to 10000
-df = pd.concat( (subject0, subject1, subject2, subject3, subject4), axis=0, ignore_index=True )
-y = df['level']
-image_names = df['image']
-index = np.arange(len(y))
+	# drop class 0 to 10000
+	df = pd.concat( (subject0, subject1, subject2, subject3, subject4), axis=0, ignore_index=True )
+	y = df['level']
+	image_names = df['image']
+	index = np.arange(len(y))
 
-# load preprocessed images
-X = joblib.load("X")
+	# load preprocessed images
+	X = joblib.load("X")
 
-X = np.swapaxes(X, 1, 3)
-print("image shape: ", X.shape)
+	X = np.swapaxes(X, 1, 3)
+	print("image shape: ", X.shape)
 
-# make train, valid, heldout splits
-train_val_indices, heldout_indices = train_test_split(index, test_size=0.3, random_state=random_seed, stratify=y)
+	# make train, valid, heldout splits
+	train_val_indices, heldout_indices = train_test_split(index, test_size=0.3, random_state=random_seed, stratify=y)
 
-train_indices, valid_indices = train_test_split(train_val_indices, test_size=0.2, random_state=random_seed, stratify=y[train_val_indices])
+	train_indices, valid_indices = train_test_split(train_val_indices, test_size=0.2, random_state=random_seed, stratify=y[train_val_indices])
 
-labels_unique, counts = np.unique(y[train_val_indices], return_counts=True)
-class_weights = np.array([1.0 / x for x in counts])
-# provide weights for samples in the training set only
-sample_weights = class_weights[y[train_indices]]
-# sampler needs to come up with training set size number of samples
-weighted_sampler = WeightedRandomSampler( weights=sample_weights, num_samples=len(train_indices), replacement=True)
+	labels_unique, counts = np.unique(y[train_val_indices], return_counts=True)
+	class_weights = np.array([1.0 / x for x in counts])
+	# provide weights for samples in the training set only
+	sample_weights = class_weights[y[train_indices]]
+	# sampler needs to come up with training set size number of samples
+	weighted_sampler = WeightedRandomSampler( weights=sample_weights, num_samples=len(train_indices), replacement=True)
 
-# Load pretrained resnet 18. Here we only take 1 block of resnet 18 and add 2 more conv layers and 1 classification layer.
-model = resnet18(pretrained=True)
-model = torch.nn.Sequential(*(list(model.children())[0:5]),
-                nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False),
-                nn.Conv2d(64, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
-                nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(32, 16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
-                nn.AdaptiveAvgPool2d(output_size=(1, 1)),
-                nn.Flatten(1, -1),
-                nn.Linear(16, 5, bias=True)
-                )
+	# Load pretrained resnet 18. Here we only take 1 block of resnet 18 and add 2 more conv layers and 1 classification layer.
+	model = resnet18(pretrained=Pre_Train)
+	if depth:
+		model = torch.nn.Sequential(*(list(model.children())[0:5]),
+										nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False),
+										nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+										nn.ReLU(inplace=True),
+										nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+										nn.ReLU(inplace=True),
+										nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+										nn.ReLU(inplace=True),
+										nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+										nn.Flatten(1, -1),
+										nn.Linear(128, 5, bias=True)
+										)
+	else:
+		model = torch.nn.Sequential(*(list(model.children())[0:5]),
+										nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False),
+										nn.Conv2d(64, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+										nn.ReLU(inplace=True),
+										nn.Conv2d(32, 16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+										nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+										nn.Flatten(1, -1),
+										nn.Linear(16, 5, bias=True)
+										)
+	# Set Dataset and dataloader
+	train_dataset = Dataset(X, y, train_indices)
+	train_dataloader = DataLoader(train_dataset, batch_size=32, sampler=weighted_sampler)
+	test_dataset = Dataset(X, y, valid_indices)
+	test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-# Set Dataset and dataloader
-train_dataset = Dataset(X, y, train_indices)
-train_dataloader = DataLoader(train_dataset, batch_size=32, sampler=weighted_sampler)
-test_dataset = Dataset(X, y, valid_indices)
-test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+	# Number of epochs
+	epochs = 50
 
-# Number of epochs
-epochs = 100
+	# Set device
+	device = torch.device('cuda:0')
 
-# Set device
-device = torch.device('cuda:0')
+	# We are using cross entropy loss as loss function
+	loss_function = nn.CrossEntropyLoss()
 
-# We are using cross entropy loss as loss function
-loss_function = nn.CrossEntropyLoss()
+	# Using adam optimizer
+	optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+	model = model.to(device)
 
-# Using adam optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
-model = model.to(device)
+	# Trainning
+	train_loss_history, valid_loss_history = [], []
 
-# Trainning
-train_loss_history, valid_loss_history = [], []
 
-lowest_loss = 9999
+	for epoch in range(epochs):
+		model.train()
+		train_loss = []
+		for batch_idx, batch in enumerate(train_dataloader):
+			X_batch = batch["data"].to(device)
+			y_batch = batch["label"].to(device)
+			optimizer.zero_grad()
+			
+			outputs = model(X_batch.float())
+			loss = loss_function(outputs, y_batch)
+			train_loss.append(loss.item())
+			loss.backward()
+			optimizer.step()
 
-for epoch in range(epochs):
-  model.train()
-  train_loss = []
-  for batch_idx, batch in enumerate(train_dataloader):
-    X_batch = batch["data"].to(device)
-    y_batch = batch["label"].to(device)
-    optimizer.zero_grad()
-    
-    outputs = model(X_batch.float())
-    loss = loss_function(outputs, y_batch)
-    train_loss.append(loss.item())
-    loss.backward()
-    optimizer.step()
+		model.eval()
+		with torch.no_grad():
+			valid_loss = []
+			y_probs_valid = torch.empty(0, 5).to(device)
+			y_true_valid, y_pred_valid = [], []
 
-  model.eval()
-  with torch.no_grad():
-    valid_loss = []
-    y_probs_valid = torch.empty(0, 5).to(device)
-    y_true_valid, y_pred_valid = [], []
+			for i, batch in enumerate(test_dataloader):
+				X_batch = batch["data"].to(device)
+				y_batch = batch["label"].to(device)
+				
+				outputs = model(X_batch.float())
+				loss = loss_function(outputs, y_batch)
+				valid_loss.append(loss.item())
 
-    for i, batch in enumerate(test_dataloader):
-      X_batch = batch["data"].to(device)
-      y_batch = batch["label"].to(device)
-      
-      outputs = model(X_batch.float())
-      loss = loss_function(outputs, y_batch)
-      valid_loss.append(loss.item())
+				_, predicted = torch.max(outputs.data, 1)
+				y_pred_valid += predicted.cpu().numpy().tolist()
+				y_probs_valid = torch.cat((y_probs_valid, outputs), 0)
+				y_true_valid += y_batch.cpu().numpy().tolist()
+		y_probs_valid = F.softmax(y_probs_valid, dim=1).cpu().numpy()
+		y_true_valid = np.array(y_true_valid)
 
-      _, predicted = torch.max(outputs.data, 1)
-      y_pred_valid += predicted.cpu().numpy().tolist()
-      y_probs_valid = torch.cat((y_probs_valid, outputs), 0)
-      y_true_valid += y_batch.cpu().numpy().tolist()
-  y_probs_valid = F.softmax(y_probs_valid, dim=1).cpu().numpy()
-  y_true_valid = np.array(y_true_valid)
+		train_loss_history.append(np.mean(train_loss))
+		valid_loss_history.append(np.mean(valid_loss))
+		
+		auc = roc_auc_score(y_true_valid, y_probs_valid, multi_class='ovr')
+		print(f"Epoch {epoch} train loss: {train_loss_history[-1]} ", f"test loss: { valid_loss_history[-1] }")
+		print("AUC: ", auc)
+		print(classification_report(y_true_valid, y_pred_valid))
+		
+		# Save model
+		state = {
+				'model_description': str(model),
+				'model_state': model.state_dict(),
+				'optimizer': optimizer.state_dict()
+		}
 
-  train_loss_history.append(np.mean(train_loss))
-  valid_loss_history.append(np.mean(valid_loss))
-  
-  auc = roc_auc_score(y_true_valid, y_probs_valid, multi_class='ovr')
-  print(f"Epoch {epoch} train loss: {train_loss_history[-1]} ", f"test loss: { valid_loss_history[-1] }")
-  print("AUC: ", auc)
-  print(classification_report(y_true_valid, y_pred_valid))
-  
-  # Save model
-  state = {
-      'model_description': str(model),
-      'model_state': model.state_dict(),
-      'optimizer': optimizer.state_dict()
-  }
+	
+		torch.save(state, f"model_{epoch}.ckpt")
 
- 
-  torch.save(state, f"model_{epoch}.ckpt")
+	# Plot loss curve
+	num_epoch = np.arange(len(train_loss_history) )
+
+	plt.plot(num_epoch, train_loss_history, label="train")
+	plt.plot(num_epoch, valid_loss_history, label="valid")
+	plt.xlabel('epoch')
+	plt.ylabel('Loss')
+	plt.title("Loss curves")
+	plt.legend()
+	plt.savefig(f"loss_curves_deep{args.deep}_pre{args.pretrain}.png")
